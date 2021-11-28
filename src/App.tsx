7398fad74b1a04a9e82bbd839542dc880f12b176
Triips-TheCoder <julian.laballe@mmibordeaux.com>
2021-11-28 14:46:01 +0100

    add form and input event

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import './App.css'
 import {BrowserRouter, Routes, Route} from "react-router-dom"
-import Home from "./pages/Home"
+import Home from "./pages/Home/Home"
 import Navbar from "./components/Navbar"
+import Register from "./pages/Register/Register"
 
 function App() {
     return (
         <BrowserRouter>
             <Navbar/>
             <Routes>
-                <Route path="/" element={<Home/>}></Route>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/register" element={<Register/>}/>
             </Routes>
         </BrowserRouter>
     )
---
+++ src/pages/Register/Register.tsx
@@ -0,0 +1,59 @@
+import './Register.css'
+import {useState} from "react"
+
+interface User {
+    name: string,
+    email: string,
+    password: string,
+    error: null | string,
+    loading: boolean
+}
+
+const Register = () => {
+    const [data, setData] = useState<User>({
+        name: '',
+        email: '',
+        password: '',
+        error: null,
+        loading: false
+    })
+
+    const {name, email, password, error, loading} = data
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const target = e.target as HTMLInputElement
+        setData({...data, [target.name]: target.value})
+    }
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        if (!name || !email || !password)
+            setData({...data, error: "Veuillez remplir tout les champs."})
+
+    }
+
+    return (
+        <section>
+            <h3>Créer un compte</h3>
+            <form className='form' onSubmit={handleSubmit}>
+                <div className="input-container">
+                    <label htmlFor="name">Nom:</label>
+                    <input type="text" id="name" name="name" value={name} onChange={handleChange}/>
+                </div>
+                <div className="input-container">
+                    <label htmlFor="email">Email:</label>
+                    <input type="text" id="email" name="email" value={email} onChange={handleChange}/>
+                </div>
+                <div className="input-container">
+                    <label htmlFor="password">Mot de passe:</label>
+                    <input type="text" id="password" name="password" value={password} onChange={handleChange}/>
+                </div>
+                <div className="btn-container">
+                    <button className="btn">S'inscrire</button>
+                </div>
+            </form>
+        </section>
+    )
+}
+
+export default Register
