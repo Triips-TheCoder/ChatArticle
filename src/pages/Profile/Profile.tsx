e4f3d7308367360ccb85a9dde087bb84e423bb8a
Triips-TheCoder <julian.laballe@mmibordeaux.com>
2021-11-30 22:45:51 +0100

    add image upload

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -2,27 +2,49 @@ import './Profile.css'
 import profileImg from '../../../src/pages/Profile/logo192.png'
 import Camera from '../../components/svg/Camera'
 import {useEffect, useState} from 'react'
-import {ref, getDownloadURL, uploadBytes} from "firebase/storage"
-import {STORAGE} from '../../firebase'
+import {ref, getDownloadURL, uploadBytes, deleteObject} from "firebase/storage"
+import {STORAGE, DB, AUTH} from '../../firebase'
+import {getDoc, doc, DocumentData, updateDoc} from 'firebase/firestore'
 
 const Profile = () => {
     const [img, setImg] = useState<File | null>(null)
+    const [user, setUser] = useState<DocumentData | null>(null)
+    let avatar: string = ""
 
     useEffect(() => {
+        getDoc(doc(DB, 'users', AUTH.currentUser!.uid)).then(docSnap => {
+            if (docSnap.exists())
+                setUser(docSnap.data())
+        })
+
         if (img) {
             // Donne un nom unique au ficher pour ne pas l'écraser et upload le fichier dans la base de donnée.
             (async () => {
-                const imgRef = ref(STORAGE, `avatar/${new Date().getTime()} - ${img.name}`)
-                const snap = await uploadBytes(imgRef, img)
+                try {
+                    if (user!.avatarPath) {
+                        await deleteObject(ref(STORAGE, user!.avatarPath))
+                    }
+                    const imgRef = ref(STORAGE, `avatar/${new Date().getTime()} - ${img.name}`)
+                    const snap = await uploadBytes(imgRef, img)
+                    const url = await getDownloadURL(ref(STORAGE, snap.ref.fullPath))
+                    await updateDoc(doc(DB, 'users', AUTH.currentUser!.uid), {
+                        avatar: url,
+                        avatarPath: snap.ref.fullPath
+                    })
+
+                    setImg(null)
+                } catch (err: any) {
+                    console.error(err.message)
+                }
             })()
         }
     }, [img])
 
-    return (
+    return user ? (
         <section className="register-login">
             <div className="profile-container">
                 <div className="img-container">
-                    <img src={profileImg} alt="avatar"/>
+                    <img src={user.avatar || profileImg} alt="avatar"/>
                     <div className="overlay">
                         <div>
                             <label htmlFor="photo">
@@ -37,14 +59,14 @@ const Profile = () => {
                     </div>
                 </div>
                 <div className="text-container">
-                    <h3>Nom d'utilisateur</h3>
-                    <p>Email de l'utilisateur</p>
+                    <h3>{user!.name}</h3>
+                    <p>{user!.email}</p>
                     <hr/>
-                    <small>Rejoindre...</small>
+                    <small>Créer le : {user.createdAt.toDate().toDateString()}</small>
                 </div>
             </div>
         </section>
-    )
+    ) : null
 }
 
 export default Profile
